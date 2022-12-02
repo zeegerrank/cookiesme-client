93d7b0fbdd7dc6b0e173932b7061493cd8a2261f
zeegerrank <zeegerrank@gmail.com>
2022-12-02 12:19:22 +0700

    create NoPage handler

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router-dom";
 // Import Pages
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import NoPage from "./pages/NoPage";
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
       <Routes>
         <Route index path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NoPage />} />
       </Routes>
     </>
   );
---
+++ src/pages/NoPage.js
@@ -0,0 +1,7 @@
+import React from "react";import { Container } from "react-bootstrap";
+
+function NoPage() {
+  return <Container>NoPage</Container>;
+}
+
+export default NoPage;
